d40604a094468f1218bff9dd867f0145a57d976f
MikeGiann <michgiann@gmail.com>
2022-02-21 16:42:44 +0200

    Add project files.

---
+++ Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using Vidly.App_Start;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        protected readonly IMapper _mapper;
+        private readonly MapperConfiguration config;
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+            config = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
+            _mapper = config.CreateMapper();
+        }
+        //GET /api/movies
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public IHttpActionResult GetMovies(string query = null)
+        {
+            var moviesQuery = _context.Movies
+               .Include(m => m.Genre)
+               .Where(m => m.NumberAvailable > 0);
+
+            if (!String.IsNullOrWhiteSpace(query))
+            {
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+            }
+            var movieDtos = moviesQuery
+               .ToList()
+               .Select(_mapper.Map<Movie, MovieDto>);
+
+            return Ok(movieDtos);
+        }
+        //GET /api/movies/1
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.ToList().Select(_mapper.Map<Movie, MovieDto>);
+            if (movie == null)
+            {
+                NotFound();
+            }
+            return Ok(_context.Movies.SingleOrDefault(m => m.Id == id));
+        }
+        //POST /api/movies
+        [HttpPost]
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var movie = _mapper.Map<MovieDto, Movie>(movieDto);
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+
+            movieDto.Id = movie.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDto);
+        }
+
+        //PUT /api/movies/1
+        [HttpPut]
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public IHttpActionResult UpdateMovie(int id, MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                BadRequest();
+            }
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movieInDb == null)
+            {
+                NotFound();
+            }
+            _mapper.Map(movieDto, movieInDb);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        //DELETE /api/movies/1
+        [HttpDelete]
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public IHttpActionResult RemoveMovie(int id)
+        {
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+            if (movieInDb == null)
+            {
+                NotFound();
+            }
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Vidly/Migrations/202112231110223_SeedUsers.cs
@@ -0,0 +1,24 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedUsers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"
+                INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'137989d0-f095-472d-8ea4-77922a16bd4f', N'admin@vidly.com', 0, N'AIrsomPh9hSF8D0WxQNAF0I286sZLRB/2cVn2mQn9iAV5mkYSzDQbTJu+657TfkKsw==', N'9609a734-5cfb-4d10-a3bc-565ca00a1649', NULL, 0, 0, NULL, 1, 0, N'admin@vidly.com')
+                INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'ad1b8a17-d335-4c9c-983b-3db68eff3f2b', N'guest@vidly.com', 0, N'AJ0s5S3nRNbR/+wV0krPdYQ7ZatB8OBZiwM1tvBTjR7pAmpou16DalMaUBLloIpKNg==', N'a89ccf62-14cc-4ba9-8038-4c07dc367c5e', NULL, 0, 0, NULL, 1, 0, N'guest@vidly.com')
+
+                INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'4fceaa82-438c-4677-8c5c-58472fd8b011', N'CanManageMovies')
+
+                INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'137989d0-f095-472d-8ea4-77922a16bd4f', N'4fceaa82-438c-4677-8c5c-58472fd8b011')
+            ");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
